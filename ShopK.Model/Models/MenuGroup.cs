b12cfcb819a519a2597402ab811ed52af8d6eca4
nguyenhuukhanhphat@gmail.com <nguyenhuukhanhphat@gmail.com>
2019-02-27 20:27:38 +0700

    Bài 6 - Them domain entity cho project

---
+++ ShopK.Model/Models/MenuGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopK.Model.Models
+{
+    [Table("MenuGroups")]
+    public class MenuGroup
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { set; get; }
+
+        [Required]
+        [MaxLength(256)]
+        public string Name { set; get; }
+
+        //Quan he khoa ngoai
+        public virtual IEnumerable<Menu> Menus { set; get; }
+    }
+}
